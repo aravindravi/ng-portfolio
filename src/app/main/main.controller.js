833632cbe615b2f6a29a93b10140d618c063cab0
Aravind Ravi <aravind_ravi@hotmail.com>
2015-10-23 23:01:53 -0700

    Home page revamp

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,11 +6,33 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($scope, $window, $rootScope) {
+  function MainController($scope, $window, $rootScope, $interval, $timeout) {
   	$scope.$on('$viewContentLoaded', function(){
       	//$(document).scroll(scrollCheck);
         angular.element($window).bind("scroll", $rootScope.scrollCheck);
         $rootScope.changeHeaderState(1);
+        var count=1;
+        var numImages=3;
+        var imgPath = 'app/main/assets/img/switcher';
+        $timeout(function() {
+          $('.cover-img').addClass('zoom');
+        },100);
+        
+        $interval(function() {
+          $('.cover-img').removeClass('zoom');
+          if(count===numImages) {
+            count=1;
+          }
+          else {
+            count++;
+          }
+          $('.cover-img').attr('src',imgPath+count.toString()+'.jpg');
+          $timeout(function() {
+            
+            $('.cover-img').addClass('zoom');
+          }, 1000);
+
+        },5000);
     });
   }
 })();
